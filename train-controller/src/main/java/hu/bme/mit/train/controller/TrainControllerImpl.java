943826b8217288976346ec2efa440b23ed078891
PDomonkos <pogi97@gmail.com>
2020-03-23 14:59:21 +0100

    controllerImpl

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,6 +7,29 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Thread thread;
+
+	/*
+	Setting the reference speed of the train works in the following way.
+			* The user can set the position of a joystick, which can be zero or a positive/negative number.
+			* In each time unit, the controller adds the position of the joystick to the current reference speed. Therefore the reference speed does not change if the position is zero, it is incremented if the position is positive and it is decremented if the position is negative.
+			* After each change, the speed limit is checked and enforced.
+	*/
+	public TrainControllerIMpl(){
+		Thread t = new Thread(){
+			public void run() {
+				while (true) {
+					try {
+						followSpeed();
+						thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		t.start();
+	}
 
 	@Override
 	public void followSpeed() {
