7d4fd35a78e39c8aa24f50a7753a52400a04bf51
PDomonkos <pogi97@gmail.com>
2020-03-30 16:40:22 +0200

    train-sensor

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -2,14 +2,17 @@ package hu.bme.mit.train.sensor;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
+	private TrainUser user;
 	private int speedLimit = 5;
 
-	public TrainSensorImpl(TrainController controller) {
+	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
+		this.user = user;
 	}
 
 	@Override
@@ -21,6 +24,15 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+
+		if (speedLimit > 500 || speedLimit < 0){
+			user.setAlarmState(true);
+		}
+		else if (speedLimit < 0.5*this.controller.getReferenceSpeed()){
+			user.setAlarmState(true);
+		} else {
+			user.setAlarmState(false);
+		}
 	}
 
 }
---
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -0,0 +1,64 @@
+package hu.bme.mit.train.sensor;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainController;
+
+public class TrainSensorTest {
+
+    private TrainSensorImpl ts;
+    private TrainUser mtu;
+    private TrainController mtc;
+
+    @Before
+    public void before() {
+        //tc = new TrainControllerImpl();
+        //tu = new TrainUserImpl(tc);
+        this.mtc = mock(TrainController.class);
+        this.mtu = mock(TrainUser.class);
+        this.ts = new TrainSensorImpl(mtc, mtu);
+    }
+
+    //tesztesetek a határértékeknél
+
+    @Test
+    public void belowLowerAbsoluteMargin(){
+        ts.overrideSpeedLimit(-1);
+        verify(mtu, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void aboveUpperAbsoluteMargin(){
+        ts.overrideSpeedLimit(501);
+        verify(mtu, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void aboveLowerAbsoluteMargin(){
+        ts.overrideSpeedLimit(1);
+        verify(mtu, times(1)).setAlarmState(false);
+    }
+
+    @Test
+    public void belowUpperAbsoluteMargin(){
+        ts.overrideSpeedLimit(499);
+        verify(mtu, times(1)).setAlarmState(false);
+    }
+
+    // 10 nek az 50%-a felett
+    @Test
+    public void aboveRelativeMargin(){
+        ts.overrideSpeedLimit(6);
+        verify(mtu, times(1)).setAlarmState(false);
+    }
+
+    // 10 nek az 50%-a alatt
+    @Test
+    public void belowRelativeMargin(){
+        ts.overrideSpeedLimit(4);
+        verify(mtu, times(1)).setAlarmState(true);
+    }
+}
